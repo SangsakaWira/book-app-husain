6d78f37c4b37f004b66c9d0dfbdf8d48da6c0cae
Sangsaka Wira <sangsakawira@gmail.com>
2019-06-30 08:48:01 +0700

    user routes available

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const bodyParser = require("body-parser")
 require("./models/mongoose")
 
 const app = express()
+app.use(require("./routes/user"))
+
+app.listen(3000,()=>{
+    console.log("Server is running!")
+})
